0e1f33ec2c4fd0dd181a58e01137b79904d894e9
Abubakirov Vladimir <abubakirov04@mail.ru>
2022-06-28 21:46:02 +0300

    Design works

--- app/src/main/java/com/example/viewtogether/create/CreateView.kt
+++ app/src/main/java/com/example/viewtogether/create/CreateView.kt
@@ -1,24 +1,67 @@
 package com.example.viewtogether.create
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.error
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.viewtogether.R
 
 @Composable
 fun CreateView(navController: NavController) {
+    var link by rememberSaveable { mutableStateOf("") }
+
     Column(
-        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(onClick = { /*TODO*/ }) {
-            Text(stringResource(id = R.string.choose_file))
+            Text(stringResource(id = R.string.choose_file), fontSize = 24.sp)
         }
+        Text(text = "or", fontSize = 24.sp, modifier = Modifier.padding(top = 16.dp))
+        Text(text = "Provide a link to youtube", fontSize = 24.sp, modifier = Modifier.padding(top = 16.dp))
+        TextField(
+            value = link,
+            label = { Text(stringResource(id = R.string.youtube_link_label)) },
+            onValueChange = {
+                link = it
+            },
+            singleLine = true,
+            modifier = Modifier
+                .width(IntrinsicSize.Max)
+                .padding(top = 16.dp),
+            placeholder = { Text(stringResource(id = R.string.youtube_link_placeholder), fontSize = 24.sp) },
+            trailingIcon = {
+                IconButton(
+                    modifier = Modifier.then(Modifier.size(24.dp)),
+                    onClick = {
+                        link = ""
+                    }
+                ) {
+                    Icon(
+                        Icons.Filled.Clear,
+                        contentDescription = "Clear field",
+                    )
+                }
+            }
+        )
     }
 }
--- app/src/main/java/com/example/viewtogether/settings/SettingsView.kt
+++ app/src/main/java/com/example/viewtogether/settings/SettingsView.kt
@@ -1,21 +1,61 @@
 package com.example.viewtogether.settings
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.viewtogether.R
+import com.example.viewtogether.ui.theme.beige
 
 @Composable
 fun SettingsView(navController: NavController) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
     ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .background(beige)
+                .fillMaxWidth().clickable(onClick = {
 
+                })
+        ) {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "Non authorised user",
+                modifier = Modifier.size(128.dp)
+            )
+            Column {
+                Text(
+                    stringResource(id = R.string.basic_family_name),
+                    modifier = Modifier.padding(top = 16.dp),
+                    fontSize = 24.sp
+                )
+                Text(
+                    stringResource(id = R.string.basic_second_name),
+                    modifier = Modifier.padding(top = 16.dp),
+                    fontSize = 24.sp
+                )
+            }
+            Icon(
+                Icons.Filled.KeyboardArrowRight,
+                contentDescription = "Go to profile",
+                modifier = Modifier
+                    .padding(top = 32.dp, start = 100.dp)
+                    .size(64.dp)
+            )
+        }
     }
 }
